84f77642c4997ccb01d773d51828498026a6f483
NicholasJoeM <120403562+NicholasJoeM@users.noreply.github.com>
2023-05-12 08:41:36 -0400

    Add files via upload

---
+++ charData.js
@@ -0,0 +1,162 @@
+module.exports = [
+  {
+    id: 0,
+    name: "Morgoth",
+    imgAddress: "https://cdnb.artstation.com/p/assets/images/images/053/991/863/large/matej-michalisko-grid-0-24-copy.jpg?1663521610",
+    health: 200,
+    attacks: [
+      {
+        id: 0,
+        damage: 25,
+      },
+      {
+        id: 1,
+        damage: 30,
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "Saruman",
+    imgAddress: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/338c48b9-9d9f-4dd8-b040-d292a819822a/d5r3omh-9c7de11a-548e-40d6-add9-7c9ab2742eb1.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzMzOGM0OGI5LTlkOWYtNGRkOC1iMDQwLWQyOTJhODE5ODIyYVwvZDVyM29taC05YzdkZTExYS01NDhlLTQwZDYtYWRkOS03YzlhYjI3NDJlYjEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.NcH-HHUIlzA0_4WXDtWaixIXMNcb4bLqhO6j8d_Q2RI",
+    health: 110,
+    attacks: [
+      {
+        id: 0,
+        damage: 15,
+      },
+      {
+        id: 1,
+        damage: 40,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Sauron",
+    imgAddress: "https://pm1.narvii.com/6782/df05d70c51c206b9facd5ca452c58c6b17a163a5v2_00.jpg",
+    health: 150,
+    attacks: [
+      {
+        id: 0,
+        damage: 30,
+      },
+      {
+        id: 1,
+        damage: 15,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Durins Bane",
+    imgAddress: "https://cdn.dribbble.com/users/6720083/screenshots/14941032/untitled-1_4x.jpg",
+    health: 155,
+    attacks: [
+      {
+        id: 0,
+        damage: 15,
+      },
+      {
+        id: 1,
+        damage: 35,
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Gothmog",
+    imgAddress: "https://pbs.twimg.com/media/Em5CxzHXEAEVqLV.jpg",
+    health: 155,
+    attacks: [
+      {
+        id: 0,
+        damage: 25,
+      },
+      {
+        id: 1,
+        damage: 25,
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Charcaroth",
+    imgAddress: "https://pbs.twimg.com/media/E1dW9l1WUAABQR2.jpg",
+    health: 135,
+    attacks: [
+      {
+        id: 0,
+        damage: 25,
+      },
+      {
+        id: 1,
+        damage: 30,
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "Smaug",
+    imgAddress: "https://qph.cf2.quoracdn.net/main-qimg-32715808d7e1d2f85491e26a7d51c6e5",
+    health: 185,
+    attacks: [
+      {
+        id: 0,
+        damage: 15,
+      },
+      {
+        id: 1,
+        damage: 40,
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "Ungoliant",
+    imgAddress: "https://i.pinimg.com/originals/ad/3f/f8/ad3ff8632a32ff351c2f13531f176b53.jpg",
+    health: 200,
+    attacks: [
+      {
+        id: 0,
+        damage: 30,
+      },
+      {
+        id: 1,
+        damage: 15,
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: "Witch King",
+    imgAddress: "https://cdna.artstation.com/p/assets/images/images/028/617/264/large/alvaro-fernandez-gonzalez-rey-brujo-de-angmar.jpg?1594983263",
+    health: 100,
+    attacks: [
+      {
+        id: 0,
+        damage: 15,
+      },
+      {
+        id: 1,
+        damage: 35,
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: "Feanor",
+    imgAddress: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5b7664c6-ffcd-4d20-8a4d-5bc0ee0ccb80/dfl9o4c-b591580a-834d-4df7-8c58-f8845b3ac3d8.png/v1/fill/w_1920,h_1920,q_80,strp/feanor_holding_a_silmaril_by_rylyn84_dfl9o4c-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTkyMCIsInBhdGgiOiJcL2ZcLzViNzY2NGM2LWZmY2QtNGQyMC04YTRkLTViYzBlZTBjY2I4MFwvZGZsOW80Yy1iNTkxNTgwYS04MzRkLTRkZjctOGM1OC1mODg0NWIzYWMzZDgucG5nIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.SoZXRBHATs8yJ1VMfv5cMKrqzH2Cxgfe53FtqE8Sit0",
+    health: 120,
+    attacks: [
+      {
+        id: 0,
+        damage: 25,
+      },
+      {
+        id: 1,
+        damage: 25,
+      },
+    ],
+  },
+];
